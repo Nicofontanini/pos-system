b5c6107d690dc815d2a2168be74235f5630225ef
nicolasfontanini <anicolasfontanini@gmail.com>
2025-04-03 20:09:44 -0300

    mejorados

---
+++ public/scripts/EmployeeLogModal.js
@@ -0,0 +1,8 @@
+ // Función para mostrar el modal de registro
+ function showEmployeeLogModal() {
+    document.getElementById('employeeLogModal').style.display = 'block';
+  }
+
+  function closeEmployeeLogModal() {
+    document.getElementById('employeeLogModal').style.display = 'none';
+  }
